35b6aae8090ee8faac16b659223a4f2d3f3d3bce
wooknick <wooknick@naver.com>
2019-08-04 16:02:28 +0900

    add navigation layout

---
+++ navigation/MessageNavigation.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from "react-navigation";
+import Message from "../screens/Messages/Message";
+import Messages from "../screens/Messages/Messages";
+
+export default createStackNavigator({
+    Message,
+    Messages
+});
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,14 +1,33 @@
+import React from "react";
 import { View } from "react-native";
-import { createBottomTabNavigator } from "react-navigation";
-import Home from "../screens/Home";
-import Notification from "../screens/Notification";
-import Profile from "../screens/Profile";
-import Search from "../screens/Search";
+import { createBottomTabNavigator, createStackNavigator } from "react-navigation";
+import Home from "../screens/Tabs/Home";
+import Notifications from "../screens/Tabs/Notifications";
+import Profile from "../screens/Tabs/Profile";
+import Search from "../screens/Tabs/Search";
+import MessagesLink from "../components/MessagesLink";
+
+const stackFactory = (initialRoute, customConfig) =>
+    createStackNavigator({
+        InitialRoute: {
+            screen: initialRoute,
+            navigationOptions: { ...customConfig }
+        }
+    });
 
 export default createBottomTabNavigator(
     {
-        Feed: { screen: Home },
-        Search,
+        Home: {
+            screen: stackFactory(Home, {
+                title: "Home",
+                headerRight: <MessagesLink />
+            })
+        },
+        Search: {
+            screen: stackFactory(Search, {
+                title: "Search"
+            })
+        },
         Add: {
             screen: View,
             navigationOptions: {
@@ -17,8 +36,16 @@ export default createBottomTabNavigator(
                 }
             }
         },
-        Notification,
-        Profile
+        Notifications: {
+            screen: stackFactory(Notifications, {
+                title: "Notifications"
+            })
+        },
+        Profile: {
+            screen: stackFactory(Profile, {
+                title: "Profile"
+            })
+        }
     },
-    { initialRouteName: "Feed" }
+    { initialRouteName: "Home" }
 );
